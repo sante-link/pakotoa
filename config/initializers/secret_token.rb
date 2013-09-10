ffecd3666292d4b3d673104233d1cd91de90da30
Romain Tartière <romain@blogreen.org>
2013-09-10 07:53:14 +0200

    Create Rails 4 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pakotoa::Application.config.secret_key_base = 'ace16f1d919a2fa362871a324b62f12a6be3d51e06e145808a84448003d3b61ddfd72c6b8a7876f2b88e5e638b1367615b8e4d0ddbe49093f51f5badf4a887ba'
