31d5bc23b2a6e0fe6c552f31d09e3f7ca9ea8b89
Romain Tartière <romain@blogreen.org>
2014-05-22 02:11:31 +0200

    Indent after last commit.

--- app/api/pakotoa/api.rb
+++ app/api/pakotoa/api.rb
@@ -18,43 +18,43 @@ module Pakotoa
     doorkeeper_for :all, except: [ :'api-docs', :'api-docs/:name' ]
 
     resource :certificates do
-    desc 'Creates a Certificate from the given Certificate Sign Request'
-    params do
-      requires :csr, type: String, desc: 'The Certificate Sign Request (CSR)'
-      requires :issuer, type: String, desc: 'The subject of the Certificate Authority (CA) who will sign the CSR'
-      optional :export_name, type: String, desc: 'The pathname to save the generated certificate to (if supported)'
-    end
-    post :sign do
-      issuer = CertificateAuthority.find_by(subject: params[:issuer])
-      raise StandardError.new("Certificate Authority not found") if issuer.nil?
-      certificate = issuer.sign_certificate_request(params[:csr], params[:export_name])
-      raise StandardError.new("Certificate not persisted: #{certificate.errors.full_messages.join("\n")}") unless certificate.persisted?
-      { certificate: certificate.certificate.to_pem }
-    end
-
-    desc 'Revokes a Certificate'
-    params do
-      requires :issuer, type: String, desc: 'The suject of the Certificate Authority (CA) who emitted the certificate to revoke'
-      optional :serial, type: String, desc: 'The serial number of the certificate to revoke (as hex string)'
-      optional :suject, type: String, desc: 'The subject of the certificate to revoke'
-    end
-    patch :revoke, protected: false do
-      issuer = CertificateAuthority.find_by(subject: params[:issuer])
-      raise StandardError.new("Certificate Authority not found") if issuer.nil?
-
-      if params[:serial] then
-        certificate = issuer.certificates.find_by(serial: params[:serial])
-      elsif params[:subject] then
-        certificate = issuer.certificates.find_by(subject: params[:subject])
-      else
-        raise StandardError.new("issuer or subject is missing")
+      desc 'Creates a Certificate from the given Certificate Sign Request'
+      params do
+        requires :csr, type: String, desc: 'The Certificate Sign Request (CSR)'
+        requires :issuer, type: String, desc: 'The subject of the Certificate Authority (CA) who will sign the CSR'
+        optional :export_name, type: String, desc: 'The pathname to save the generated certificate to (if supported)'
       end
-      raise StandardError.new("certificate not found") if certificate.nil?
+      post :sign do
+        issuer = CertificateAuthority.find_by(subject: params[:issuer])
+        raise StandardError.new("Certificate Authority not found") if issuer.nil?
+        certificate = issuer.sign_certificate_request(params[:csr], params[:export_name])
+        raise StandardError.new("Certificate not persisted: #{certificate.errors.full_messages.join("\n")}") unless certificate.persisted?
+        { certificate: certificate.certificate.to_pem }
+      end
 
-      certificate.update_attributes!(revoked_at: Time.now.utc)
+      desc 'Revokes a Certificate'
+      params do
+        requires :issuer, type: String, desc: 'The suject of the Certificate Authority (CA) who emitted the certificate to revoke'
+        optional :serial, type: String, desc: 'The serial number of the certificate to revoke (as hex string)'
+        optional :suject, type: String, desc: 'The subject of the certificate to revoke'
+      end
+      patch :revoke, protected: false do
+        issuer = CertificateAuthority.find_by(subject: params[:issuer])
+        raise StandardError.new("Certificate Authority not found") if issuer.nil?
 
-      { status: 'OK' }
-    end
+        if params[:serial] then
+          certificate = issuer.certificates.find_by(serial: params[:serial])
+        elsif params[:subject] then
+          certificate = issuer.certificates.find_by(subject: params[:subject])
+        else
+          raise StandardError.new("issuer or subject is missing")
+        end
+        raise StandardError.new("certificate not found") if certificate.nil?
+
+        certificate.update_attributes!(revoked_at: Time.now.utc)
+
+        { status: 'OK' }
+      end
     end
 
     add_swagger_documentation base_path: 'api', mount_path: 'api-docs', api_version: 'v1', hide_documentation_path: true
